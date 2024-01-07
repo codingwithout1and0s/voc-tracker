cbe3394e4656d176960120e5ae5a068674be832c
codingwithout1and0s <codingwithout1and0s@gmail.com>
2024-01-07 04:11:38 -0600

    Adjusted Stats to display avgCSAT

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,11 +6,11 @@ import Progress from 'react-circle-progress-bar'
 
 const Stats = () => {
 
-    const { AvgCsat } = useContext(CSATContext);
+    const { avgCsat } = useContext(CSATContext);
 
-    const subtitle_bar1 = 'Bar 1';
-    const subtitle_bar2 = 'Bar 2';
-    const subtitle_bar3 = 'Bar 3';
+    const subtitle_bar1 = `${avgCsat.toFixed(2)} / 5.00`
+    const subtitle_bar2 = `${avgCsat.toFixed(2)} / 5.00`
+    const subtitle_bar3 = `4.00 / 5.00`
 
     const navigate = useNavigate();
 
@@ -18,22 +18,25 @@ const Stats = () => {
         navigate("/full-stats");
     }
 
+    const prog1 = (avgCsat / 5) * 100;
+    const prog2 = (avgCsat / 5) * 100;
+    const prog3 = (4 / 5) * 100;
+
     return (
         <div onClick={handleClick}>
             <Container fluid className='mb-2 bg-secondary py-3'>
-                <h1>AVG CSAT: {AvgCsat}</h1>
                 <Row className='text-center text-white'>
                     <Col>
-                        <h4>CSAT MTD</h4>
-                        <Progress progress={75} subtitle={subtitle_bar1} className='mx-auto'/>
+                        <h4>Current CSAT</h4>
+                        <Progress progress={prog1} subtitle={subtitle_bar1} className='mx-auto'/>
                     </Col>
                     <Col>
                         <h4>CSAT MTD</h4>
-                        <Progress progress={50} subtitle={subtitle_bar2} className='mx-auto'/>
+                        <Progress progress={prog2} subtitle={subtitle_bar2} className='mx-auto'/>
                     </Col>
                     <Col>
                         <h4>CSAT YTD</h4>
-                        <Progress progress={25} subtitle={subtitle_bar3} className='mx-auto'/>
+                        <Progress progress={prog3} subtitle={subtitle_bar3} className='mx-auto'/>
                     </Col>
                 </Row>
             </Container>
