4125d8bfeb6435982e3d52ab13c54a1592059358
Tang Newyen <codingwithout1and0s@gmail.com>
2024-03-25 19:06:46 -0500

    small ui fixes

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,23 +1,39 @@
 import { useState, useEffect } from 'react'
 import { Container, ListGroup } from 'react-bootstrap'
 import { useAdminTitle } from '../Contexts/AdminTitleContext'
+import axios from 'axios';
 
 function Settings() {
   const { setAdminTitle } = useAdminTitle();
 
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    await axios.get('http://localhost:8080/api/')
+      .then(res => {
+        setUsers(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  const deleteUser = async (id) => {
+    try {
+      const { data } = await axios.delete(`http://localhost:8080/api/{id}`);
+      setUsers(users.filter(users => users.id !== id));
+      console.log("All users", data);
+    } catch (error) {
+      console.log("Caught error: ", error);
+    }
+  }
+
   useEffect(() => {
     setAdminTitle("Settings");
+    getUsers();
   }, [])
 
   const [editMode, setEditMode] = useState(false);
-
-  const agentArray = [
-    {"name": "Mike", "email": "mike@examples.com"},
-    {"name": "Jane", "email": "jane@examples.com"},
-    {"name": "Phil", "email": "phil@examples.com"},
-    {"name": "Harry", "email": "harry@examples.com"},
-    {"name": "Scarlet", "email": "scarlet@examples.com"},
-    {"name": "George", "email": "george@examples.com"}];
 
   return (
     <section id="settings">
@@ -26,13 +42,14 @@ function Settings() {
           <p>Agent List</p>
 
           <ListGroup className="d-flex flex-column">
-            {agentArray.map((agent, index) => {
+            {users.map((agent, index) => {
                 return <ListGroup.Item key={index} className='d-flex flex-row'>
-                {agent.name} <br />
-                {editMode ?  <input type='text' placeholder={agent.email} className='mx-5'/>: <p className='mx-5'>{agent.email}</p>}
+                {agent.firstName + " " + agent.lastName} <br />
+                {editMode ?  <input type='text' placeholder={agent.login} className='mx-5'/>: <p className='mx-5'>{agent.login}</p>}
                 <button className="btn btn-danger mx-5" onClick={() => {
                   setEditMode(current => !current)}}>Edit</button>
                 <button className="mx-5">Reset Password</button>
+                <button onClick={()=> deleteUser(agent.id)}>X</button>
               </ListGroup.Item>
             })}
           </ListGroup>
